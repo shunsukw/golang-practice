caa1a1e29f8679b10cfdc3c0db1ca451c8c28a74
shunsukw <ww.shunsuke@gmail.com>
2018-08-21 20:05:17 +0900

    implemented all of the contents

--- dino/dino.go
+++ dino/dino.go
@@ -9,7 +9,10 @@ import (
 )
 
 type configuration struct {
-	ServerAddress string `json:"webserver"`
+	ServerAddress      string `json:"webserver"`
+	DatabaseType       uint8  `json:"databasetype"`
+	DatabaseConnection string `json:"dbconnection"`
+	FrontEnd           string `json:"frontend"`
 }
 
 func main() {
@@ -20,5 +23,5 @@ func main() {
 	config := new(configuration)
 	json.NewDecoder(file).Decode(config)
 	log.Println("Starting on web server on address", config.ServerAddress)
-	dinowebportal.RunWebPortal(config.ServerAddress)
+	dinowebportal.RunWebPortal(config.DatabaseType, config.ServerAddress, config.DatabaseConnection, config.FrontEnd)
 }
